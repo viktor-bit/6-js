afd53e47ba8f3d62259494f0ff43fecf395eb940
viktor <58216240+viktor-bit@users.noreply.github.com>
2019-12-12 19:36:27 +0200

    js bandimai

--- js/main.js
+++ js/main.js
@@ -1,10 +1,51 @@
 console.log("Labas rytas");
 console.log("sveiki");
 
-
-let a=0;
-const b=11;
-for (let  a= 0; a <= b; a++) {
-    
-    
+let daliklis=3;
+const a1=-25;
+const b1=11;
+let c=0;
+let a=a1; 
+const b=b1;
+for ( a ; a <= b; a++) { 
+  if( a % daliklis === 0 )
+    c++;   
 }
+console.log('skaiciu intervale tarp ' + a1 + ' ir ' + b1 + ', besidalinanciu be liekannos is ' + daliklis + ' yra ' + c + ' vienetai');
+a=a1;
+
+daliklis=daliklis + 2;
+    if(daliklis<= 7 );
+        for ( a ; a <= b; a++) { 
+        if( a % daliklis === 0 )
+        c++;   
+  }
+  console.log('skaiciu intervale tarp ' + a1 + ' ir ' + b1 + ', besidalinanciu be liekannos is ' + daliklis + ' yra ' + c + ' vienetai');
+  daliklis=daliklis + 2;
+  if(daliklis<= 7 );
+      for ( a ; a <= b; a++) { 
+      if( a % daliklis === 0 )
+      c++;   
+}
+
+
+
+  console.log('skaiciu intervale tarp ' + a1 + ' ir ' + b1 + ', besidalinanciu be liekannos is ' + daliklis + ' yra ' + c + ' vienetai');
+
+
+
+
+ 
+  
+  
+// const daliklis=3
+// let c=0;
+// let a=0;
+// const b=11
+
+
+
+
+
+// console.log('skaiciu intervale tarp ' + a + ' ir ' + b + ', besidalinanciu be liekannos is ' + daliklis + ' yra ' + c + ' vienetai');
+ 
