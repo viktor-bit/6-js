5b305248124db71949c604837aa825548dbe4393
viktor <58216240+viktor-bit@users.noreply.github.com>
2019-12-13 10:21:11 +0200

    js bandimai 1

--- js/main.js
+++ js/main.js
@@ -3,15 +3,14 @@ console.log("sveiki");
 
 let daliklis=3;
 const a1=-25;
-const b1=11;
+const b=11;
 let c=0;
 let a=a1; 
-const b=b1;
 for ( a ; a <= b; a++) { 
   if( a % daliklis === 0 )
     c++;   
 }
-console.log('skaiciu intervale tarp ' + a1 + ' ir ' + b1 + ', besidalinanciu be liekannos is ' + daliklis + ' yra ' + c + ' vienetai');
+console.log('skaiciu intervale tarp ' + a1 + ' ir ' + b + ', besidalinanciu be liekannos is ' + daliklis + ' yra ' + c + ' vienetai');
 a=a1;
 
 daliklis=daliklis + 2;
@@ -20,7 +19,7 @@ daliklis=daliklis + 2;
         if( a % daliklis === 0 )
         c++;   
   }
-  console.log('skaiciu intervale tarp ' + a1 + ' ir ' + b1 + ', besidalinanciu be liekannos is ' + daliklis + ' yra ' + c + ' vienetai');
+  console.log('skaiciu intervale tarp ' + a1 + ' ir ' + b + ', besidalinanciu be liekannos is ' + daliklis + ' yra ' + c + ' vienetai');
   daliklis=daliklis + 2;
   if(daliklis<= 7 );
       for ( a ; a <= b; a++) { 
@@ -30,22 +29,18 @@ daliklis=daliklis + 2;
 
 
 
-  console.log('skaiciu intervale tarp ' + a1 + ' ir ' + b1 + ', besidalinanciu be liekannos is ' + daliklis + ' yra ' + c + ' vienetai');
+  console.log('skaiciu intervale tarp ' + a1 + ' ir ' + b + ', besidalinanciu be liekannos is ' + daliklis + ' yra ' + c + ' vienetai');
+
+function daugyba(m,n){
+    if(typeof m !== 'number'){
+        return ''
+    }
 
 
+    
+    )
+        return m * n;
+}
 
-
- 
-  
-  
-// const daliklis=3
-// let c=0;
-// let a=0;
-// const b=11
-
-
-
-
-
-// console.log('skaiciu intervale tarp ' + a + ' ir ' + b + ', besidalinanciu be liekannos is ' + daliklis + ' yra ' + c + ' vienetai');
- 
+console.log( daugyba(2,2));
+console.log( daugyba("50",2));
